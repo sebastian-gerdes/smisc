5840f12843a98eb2320376283eb50d60752d08f3
Sebastian Gerdes <sebastian.gerdes@posteo.de>
2020-03-10 19:56:54 +0100

    elaborated function "square" with documentation, added a licence und edited the description

--- R/square.R
+++ R/square.R
@@ -1,3 +1,14 @@
+#' Square a numeric argument
+#' 
+#' This function yields the square of a numeric argument
+#' 
+#' @param x numeric
+#' 
+#' @return numeric
+#' @export
+#' @examples 
+#' square(7)
+
 square <- function(x) {
   return(x ^ 2)
 }
