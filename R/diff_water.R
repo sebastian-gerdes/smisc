b0f4f7ef58816892fee121f32a2e3c003b144d06
sebastian.gerdes <sebastian.gerdes@tu-dresden.de>
2024-10-23 08:48:03 +0200

    - more explicite argument names in function diff_water

--- R/diff_water.R
+++ R/diff_water.R
@@ -1,10 +1,10 @@
 #' Calculate the difference in wawter volume given temperature and humidity inside and outside
 #'
-#' @param T_out temperature outside in degress celsius
-#' @param T_in temperature inside in degress celsius
-#' @param h_out relative humidity inside in percent
-#' @param h_in relative humidity outside in percent
-#' @param volume Volume that is exchanged
+#' @param temperature_outside temperature outside in degress celsius
+#' @param temperature_inside temperature inside in degress celsius
+#' @param relative_humidity_outside relative humidity outside in percent
+#' @param relative_humidity_inside  relative humidity inside in percent
+#' @param volume Volume in cubic meters that is exchanged
 #'
 #' @return difference in water content before and after changing the air
 #' @export
@@ -13,7 +13,7 @@
 #' diff_water(8, 80, 19, 70)
 
 
-diff_water <- function(T_out, h_out, T_in, h_in, volume = 300) {
-  volume * (absolute_humidity(T_out, h_out) - absolute_humidity(T_in, h_in))
+diff_water <- function(temperature_outside, relative_humidity_outside, temperature_inside, relative_humidity_inside, volume = 300) {
+  volume * (absolute_humidity(temperature_outside, relative_humidity_outside) - absolute_humidity(temperature_inside, relative_humidity_inside))
 }
 
